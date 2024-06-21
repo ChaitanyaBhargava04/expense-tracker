0b7aa2633756b6f112e8eb95a01f4e675c569fa5
ChaitanyaBhargava04 <bhargavachaitanya45@gmail.com>
2024-06-21 19:20:28 +0530

    intial commit
    update

--- frontend/my-app/src/header/header.js
+++ frontend/my-app/src/header/header.js
@@ -20,7 +20,7 @@ function Header() {
           <a class="nav-link active" aria-current="page" href="/aboutus">About us</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/process">Process</a>
+          <a class="nav-link disabled" aria-current="page" href="/process" >Process</a>
         </li>
       </ul>
       <form class="d-flex" role="search">
